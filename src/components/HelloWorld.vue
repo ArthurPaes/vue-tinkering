9f0d4609575c1d7df6d7780ea5a374e8a4e8cd48
ArthurPaes <arthur.sldp@gmail.com>
2024-01-26 00:23:15 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+
+defineProps<{
+  msg: string
+}>()
+const counterStore = useCounterStore()
+const {count, doubleCount} = storeToRefs(counterStore)
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+    <div>count is {{ count }}</div>
+    <div>doubleCount is {{ doubleCount }}</div>
+    <button @click="()=>{
+      counterStore.increment()
+    }">incrementa</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
